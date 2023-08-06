5eda9e2a39407a0c936ee28f8cdc7a6f072f6ec9
sotoblanco <sotoblanco263542@gmail.com>
2023-08-06 13:47:26 +0000

    fix files and update pandas code

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 midterm = pd.read_csv('midterm_data.csv')
-midterm = midterm.drop(midterm.tail(10).index)
+midterm = midterm.drop(midterm.tail(250).index)
 print('Midterm data')
 print(midterm.tail(1))
 midterm.to_csv('midterm_data.csv', index=False)
@@ -9,7 +9,7 @@ midterm.to_csv('midterm_data.csv', index=False)
 print()
 
 nq = pd.read_csv('NQ_market_profile_master_vol_close.csv')
-nq = nq.drop(nq.tail(10).index)
+nq = nq.drop(nq.tail(250).index)
 print('NQ data')
 print(nq.tail(1))
 nq.to_csv('NQ_market_profile_master_vol_close.csv', index=False)
